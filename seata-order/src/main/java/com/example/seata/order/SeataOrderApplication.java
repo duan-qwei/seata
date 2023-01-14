f8e3ee3ae64a33efd786b334d006e952aabce524
duanqiangwei <duanqwei@163.com>
2023-01-14 19:22:32 +0800

    初始化项目

---
+++ seata-order/src/main/java/com/example/seata/order/SeataOrderApplication.java
@@ -0,0 +1,15 @@
+package com.example.seata.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SeataOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeataOrderApplication.class, args);
+    }
+
+}
