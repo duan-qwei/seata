ab73d168397c488c276e72c7d5c2602d8227748c
duanqiangwei <duanqwei@163.com>
2023-01-21 23:43:05 +0800

    解决nacos共享配置的问题

--- seata-order/src/main/java/com/duan/seata/order/controller/OrderController.java
+++ seata-order/src/main/java/com/duan/seata/order/controller/OrderController.java
@@ -1,17 +1,20 @@
 package com.duan.seata.order.controller;
 
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author duanqiangwei
  * @since 2023-01-18
  */
 @RestController
+@RefreshScope
 public class OrderController {
+
 }
 
